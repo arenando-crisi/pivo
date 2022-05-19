a3bcd5ec03516960dc97158a81013b5587ad2750
scardinanoire <andre.corsi@outlook.it>
2022-05-19 15:16:43 +0200

    video sfondo

--- js/animations.js
+++ js/animations.js
@@ -5,11 +5,19 @@ function burgertox(x) {
 }
 
 function openNav() {
-
     cont++;
     if(cont%2==0){
         document.getElementById("myNav").style.width = "0%";
+        document.getElementById("scrittemenù1").style.opacity = 0;
+        document.getElementById("scrittemenù2").style.opacity = 0;
     }else{
         document.getElementById("myNav").style.width = "100%";
+        document.getElementById("scrittemenù1").style.opacity = 100;
+        document.getElementById("scrittemenù2").style.opacity = 100;
     }
+}//chiedere come togliere il brutto effetto sulle scritte
+
+function scrollpos(){
+    setInterval(scrollpos,100);
+    console.log(window.scrollY);
 }
