2495b323c882daaa7b3fe4c17fc8aa31193a0a6c
scardinanoire <andre.corsi@outlook.it>
2022-05-23 18:47:46 +0200

    23/05 v2

--- js/scroller.js
+++ js/scroller.js
@@ -4,7 +4,18 @@ gsap.to(".header", {
     y: '10vh',
     duration: 0.4,
     scrollTrigger: {
-        trigger: ".trigger1",
+        trigger: ".triggerheader",
+        toggleActions: 'restart none none reverse',
+        start: "top 0%",
+        end: "bottom 0%",
+    }
+})
+
+gsap.fromTo(".hero",{backgroundColor: '#FA4928'},{
+    backgroundColor: '#000000',
+    duration: 0.4,
+    scrollTrigger: {
+        trigger: ".trigger7",
         toggleActions: 'restart none none reverse',
         start: "top 0%",
         end: "bottom 0%",
@@ -19,6 +30,106 @@ gsap.fromTo(".hero",{backgroundColor: '#A07EFF'},{
         toggleActions: 'restart none none reverse',
         start: "top 0%",
         end: "bottom 0%",
-}})
+    }
+})
+
+gsap.to(".claim", {
+    opacity: 0,
+    duration: 0.4,
+    scrollTrigger: {
+        trigger: ".trigger2",
+        toggleActions: 'restart none none reverse',
+        start: "top 0%",
+        end: "bottom 0%",
+        scrub: true,
+    }
+})
+
+gsap.to(".titolo", {
+    opacity: 0,
+    duration: 0.4,
+    scrollTrigger: {
+        trigger: ".trigger5",
+        toggleActions: 'restart none none reverse',
+        start: "top 0%",
+        end: "bottom 0%",
+        scrub: true,
+    }
+})
+
+gsap.fromTo(".titolo",{fontSize:'20vw'}, {
+    fontSize: '8vW',
+    duration: 0.4,
+    scrollTrigger: {
+        trigger: ".trigger3",
+        toggleActions: 'restart none none reverse',
+        start: "top 0%",
+        end: "bottom 0%",
+        scrub: true,
+    }
+})
+
+gsap.to(".claim2", {
+    opacity: 1,
+    duration: 0.4,
+    scrollTrigger: {
+        trigger: ".trigger4",
+        toggleActions: 'restart none none reverse',
+        start: "top 0%",
+        end: "bottom 0%",
+        scrub: true,
+    }
+})
+
+gsap.fromTo(".claim2",{opacity: 1}, {
+    opacity: 0,
+    duration: 0.4,
+    scrollTrigger: {
+        trigger: ".trigger5",
+        toggleActions: 'restart none none reverse',
+        start: "top 0%",
+        end: "bottom 0%",
+        scrub: true,
+    }
+})
+
+gsap.fromTo(".video", {width:'60vw'}, {
+    width:'98vw',
+    duration: 0.4,
+    scrollTrigger: {
+        trigger: ".trigger6",
+        toggleActions: 'restart none none reverse',
+        start: "top 0%",
+        end: "bottom 0%",
+        scrub: true,
+    }
+})
+
+gsap.fromTo(".video", {width:'43vw'}, {
+    width:'60vw',
+    duration: 0.4,
+    scrollTrigger: {
+        trigger: ".trigger3",
+        toggleActions: 'restart none none reverse',
+        start: "top 0%",
+        end: "bottom 0%",
+        scrub: true,
+    }
+})
+
+gsap.fromTo(".video", {position:'fixed', top: '1vh'}, {
+    position: 'absolute',
+    top:'131vh',
+    duration: 0.4,
+    scrollTrigger: {
+        trigger: ".trigger7",
+        toggleActions: 'restart none none reverse',
+        start: "top 0%",
+        end: "bottom 0%",
+        scrub: true,
+    }
+})
+
+
   
 
