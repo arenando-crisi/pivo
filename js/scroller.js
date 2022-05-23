121f54d311672aece9c911b263813f34c94c2410
scardinanoire <andre.corsi@outlook.it>
2022-05-23 12:51:37 +0200

    23/05 v1

--- js/scroller.js
+++ js/scroller.js
@@ -1,75 +1,24 @@
 gsap.registerPlugin(ScrollTrigger);
 
-gsap.to(".titolo", {
-    scale: 0.75,
-    y: 470,
-    duration: 1,
+gsap.to(".header", {
+    y: '10vh',
+    duration: 0.4,
     scrollTrigger: {
         trigger: ".trigger1",
-        markers:true,
-        start: "top 50%",
-        end: "bottom 50%",
-        scrub: true,
+        toggleActions: 'restart none none reverse',
+        start: "top 0%",
+        end: "bottom 0%",
     }
 })
 
-gsap.to(".claim", {
-    duration: 1,
-    opacity: 0,
-    scrollTrigger: {
-        trigger: ".trigger3",
-        markers:true,
-        start: "top 50%",
-        end: "bottom 50%",
-        scrub: true,
-    }
-})
-
-gsap.to(".tagline1", {
-    duration: 1,
-    opacity: 1,
+gsap.fromTo(".hero",{backgroundColor: '#A07EFF'},{
+    backgroundColor: '#FA4928',
+    duration: 0.4,
     scrollTrigger: {
         trigger: ".trigger1",
-        markers:true,
-        start: "top 50%",
-        end: "bottom 50%",
-        scrub: true,
-    }
-})
+        toggleActions: 'restart none none reverse',
+        start: "top 0%",
+        end: "bottom 0%",
+}})
+  
 
-gsap.to(".tagline2", {
-    duration: 1,
-    opacity: 1,
-    scrollTrigger: {
-        trigger: ".trigger2",
-        markers:true,
-        start: "top 50%",
-        end: "bottom 50%",
-        scrub: true,
-    }
-})
-
-gsap.to(".video", {
-    scaleX: 0.2,
-    scaleY: 0.6,
-    duration: 1,
-    scrollTrigger: {
-        trigger: ".trigger2",
-        markers:true,
-        start: "top 50%",
-        end: "bottom 50%",
-        scrub: true,
-    }
-})
-
-gsap.to(".buttonbig", {
-    opacity:  1,
-    duration: 1,
-    scrollTrigger: {
-        trigger: ".trigger2",
-        markers:true,
-        start: "top 50%",
-        end: "bottom 50%",
-        scrub: true,
-    }
-})
